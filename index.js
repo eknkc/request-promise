b58f731505523be2996f9ad9b505c540bbeb4b48
Ekin Koc <ekin@eknkc.com>
2016-01-18 00:40:16 +0200

    Initial

---
+++ index.js
@@ -0,0 +1,24 @@
+var request = require("request")
+
+module.exports = function(options) {
+  return new Promise(function(res, rej) {
+    var length = 0
+      , sizeLimit = options.sizeLimit
+
+    var req = request(options, function (err, response, body) {
+      if (err || response.statusCode != 200)
+        return rej(err || new Error("Response status: " + response.statusCode));
+
+      res(body);
+    }).on("data", function(data) {
+      length += data.length;
+
+      if (sizeLimit && length > sizeLimit) {
+        req.abort();
+        rej(new Error("Response too large."));
+      }
+    });
+  });
+};
+
+module.exports({url: 'http://onedio.com'}).then(console.log)
